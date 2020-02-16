736ff1184fe8399fcd0c3c303fdd29097e850595
jordanrob709 <jordanrob709@gmail.com>
2020-02-16 12:08:26 +0300

    initial commit

---
+++ pageee/pages/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+
+from .views import (
+    HomePageView,
+    AboutPageView
+)
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about')
+]
